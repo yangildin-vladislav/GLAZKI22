175d8a85133f0e15ebe8b2c640b8921a913b57a5
Vladislav Yangildin <dimbik@list.ru>
2023-10-25 18:45:43 +0500

    10 LABA

---
+++ Documents/И - DeskBar/ALL/C++ Програмирование/Разработка програмных модулей/YANGILDIN PROJECT 9 LABA/YANGILDIN_GLAZKI_PROJECT/YANGILDIN_GLAZKI_PROJECT/AgentPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YANGILDIN_GLAZKI_PROJECT
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
